3acb13a24557d438d6962eff3fa71082affc27ee
alexiscaulliez-ux <alexis.caulliez@request.network>
2026-02-16 18:55:36 +0100

    Added GTM script to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PGPV7DWN'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 createRoot(document.getElementById("root")!).render(<App />);
